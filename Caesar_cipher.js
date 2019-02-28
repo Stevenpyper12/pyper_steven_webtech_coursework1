ea6d1e99eee512dc2fcb5f58c0b9f64ea6743533
steven <stevenpyper12@gmail.com>
2019-02-28 14:27:49 +0000

    finished caesar_cipher

--- Caesar_cipher.js
+++ Caesar_cipher.js
@@ -1,21 +1,22 @@
 charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 numchars= charset.length;
 
-function Caesar(){
+function Caesar(type){
 	//document.getElementById("output").innerHTML = "<p>this is a different test</p>"
-	
+	alert(type)
 	//defines how many shifts there should be to the chararcter set
-	var key = 0;
-	var plaintext = "";
-	var cipher = "";
-	
-	key = document.getElementById("inputkey").value;
-	plaintext = document.getElementById("inputtext").value;
-	
-	Caesar_encrypt(plaintext,key);
+	if(type == '0')
+	{
+		Caesar_encrypt();
+	}else
+	if(type == '1'){
+		Caesar_decrypt();
+	}
 }
 
 function Caesar_encrypt(plaintext,key){
+	key = document.getElementById("inputkey").value;
+	plaintext = document.getElementById("inputtext").value;
 	plaintext = plaintext.toUpperCase();
 	var message = "encrypting the text - " + plaintext + "with a key of -" + key;
 	document.getElementById("outputsoutput").innerHTML = message;
@@ -51,4 +52,36 @@ function Caesar_encrypt(plaintext,key){
 		ciphertext = ciphertext+newletter;	
 	}
 	document.getElementById("outputsoutput").innerHTML = "encrypted message is :" +ciphertext;
+}
+
+function Caesar_decrypt(){
+	key = document.getElementById("inputkey").value;
+	ciphertext = document.getElementById("inputtext").value;
+	ciphertext = ciphertext.toUpperCase();
+	var message = "decrypting the text - " + ciphertext + "with a key of -" + key;
+	document.getElementById("outputsoutput").innerHTML = message;
+	var plaintext = "";
+	var newletter = "";
+	
+	
+	for(ch in ciphertext)
+	{
+		if(charset.includes(ciphertext[ch]))
+		{
+			
+			var newcharposition = +charset.indexOf(ciphertext[ch]) - +key;
+			if(newcharposition < 0)
+			{
+				newletter=charset.charAt(newcharposition+numchars);
+			}else
+			{
+				newletter=charset.charAt(newcharposition);
+			}
+		}else{
+			newletter= ciphertext[ch]
+		}
+		plaintext = plaintext+newletter;	
+	}
+	document.getElementById("outputsoutput").innerHTML = "decrypted message is :" +plaintext;
+
 }
--- changecipher.js
+++ changecipher.js
@@ -18,8 +18,10 @@ function testadding0(){
 
 function testadding1(){
 	document.getElementById("output").innerHTML = "<p><input type='text' id='inputtext' value=''>enter text you want encrypted or decrypted</p>"
-	document.getElementById("output").innerHTML += "<p><input type='text' id='inputkey' value=''>enter how many times you want the alphabet offset(key)(can be positive or negative)</p>"
-	document.getElementById("output").innerHTML += "<button onclick='Caesar()'>values ready?</button>"
+	document.getElementById("output").innerHTML += "<p><input type='text' id='inputkey' value=''>enter how many times you want the alphabet offset(key)(can be positive or negative,your key is the exact same for encrpytion and decyption(6 while encryption and 6 while decypting )</p>"
+	document.getElementById("output").innerHTML += "<button onclick='Caesar(\"0\")'>Encrypt</button>"
+	document.getElementById("output").innerHTML += "<button onclick='Caesar(\"1\")'>Decrypt(key Known)</button>"
+
 }
 function testadding2(){
 	document.getElementById("output").innerHTML = "<p>this is a test</p>"
