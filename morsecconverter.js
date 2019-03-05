5800aba193518c7727bcd5a8c3b6793c5765a7d8
steven <stevenpyper12@gmail.com>
2019-03-05 15:31:48 +0000

    added sound to morse in the most bodgy way possible

--- morsecconverter.js
+++ morsecconverter.js
@@ -86,9 +86,7 @@ function texttomorse()
 	}
 	 
 	document.getElementById("outputsoutput").innerHTML = "the morse text is :" + morsetext 
-	test = document.getElementById("outputsoutput").value
-	test=test.split(":")
-	alert(test[0])
+
 
 }
 
@@ -97,7 +95,6 @@ function morsetotext()
 	var plaintext = ""
 	var morsetext = document.getElementById("inputmorse").value.toUpperCase();
 	var singlemorse = ""
-	var actualspace = false
 	for(ch in morsetext)
 	{
 		if(morsetext[ch] == " " && morsetext[+ch - +1] == " ")
@@ -139,10 +136,171 @@ function morsetotext()
 
 }
 
+function playmorse()
+{
+	var context = new (window.AudioContext || window.webkitAudioContext)();
+	var oscillator = context.createOscillator();
+	oscillator.connect(context.destination);
+	oscillator.frequency.value = 0;
+	oscillator.start(0);
+	var morsetext = document.getElementById("inputmorse").value.toUpperCase();
+	var i = 0;
+	var soundjustplayed = 0;
+	var counter = 1000;
+	var dashjustplayed = 0;
+	
+	var isfinished = setInterval(function()
+	{
+		if(soundjustplayed == 1)
+		{
+			if(morsetext[i-1] == "-" && dashjustplayed == 0)
+			{
+				dashjustplayed = 1;
+			}else
+			{
+			oscillator.frequency.value = 0;
+			//isfinished = setInterval(function,5000);
+			soundjustplayed = 0;
+			dashjustplayed = 0;
+			}
+		}else
+		{
+			if(i >= morsetext.length)
+			{
+				oscillator.frequency.value = 0;
+				clearInterval(isfinished);
+			}else
+			{
+			
+				if(morsetext[i] == ".")
+				{
+					oscillator.frequency.value = 1000;
+					//isfinished = setInterval(function{},500);
+					
+				}else
+				if(morsetext[i] == "-"){
+					oscillator.frequency.value = 500;
+					//isfinished = setInterval(function,5000);
+					
+				}else
+				{
+					oscillator.frequency.value = 0;
+					//isfinished = setInterval(function,10000);
+					
+				}
+			}
+			i++
+			soundjustplayed = 1;
+		}
+	},500)
+	
+}	
+
+
+	
+	
+	
+	/*for(ch in morsetext)
+	{
+		//alert(morsetext[ch]);
+		if(morsetext[ch] == ".")
+		{
+			timeoutthing(".",1000,oscillator)
+		}else
+		if(morsetext[ch] == "-")
+		{
+			timeoutthing("-",1000,oscillator)
+			
+		}else
+		{
+			timeoutthing(" ",1000,oscillator)
+		}
+	}
+
+
+}*/
+/*
+function timeoutthing(oscillator)
+{
+	setTimeout(playsound,1000)
+
+}
+function playsound(character,oscillator)
+{
+	if(character == ".")
+	{
+		oscillator.frequency.value = 1000;
+		
+	}else
+	if(character == "-"){
+		oscillator.frequency.value = 500;
+		
+	}else
+	{
+		oscillator.frequency.value = 0;
+	}
+}
+*/ 
+	
+/*
 function playmorse(){
-	var context
-	window.AudioContext = window.AudioContext
-	context = new AudioContext()
+	
+	var context = new (window.AudioContext || window.webkitAudioContext)();
+	var dottime = 100;
+	var dashtime = 300;
+	var soundpitch = 550;
+	var dashsound = ToneSound(context,dashtime,soundpitch);
+	var source = context.createBufferSource();
+	var source2 = context.createBufferSource();
+	source.buffer = dashsound;
+	source.connect(context.destination)
+	var dottime = ToneSound(context,dottime,soundpitch)
+	source2.buffer = dottime;
+	source2.connect(context.destination)
+	var allmorse = [];
+	source.volume = 0;
+	source2.volume = 0;
+	source.start(0);
+	source2.start(0);
+	
+	
+	var morsetext = document.getElementById("inputmorse").value.toUpperCase();
+	for(ch in morsetext)
+	{
+		//alert(morsetext[ch]);
+		if(morsetext[ch] == ".")
+		{
+			source.volume = 0.5;
+			setTimeout(1000)
+			source.volume = 0;
+		}else
+		if(morsetext[ch] == "-"){
+			source2.volume = 0.5;
+			setTimeout(1000)
+			source.volume = 0;
+		}else
+		{
+			setTimeout(1000)
+		}
+	}
 	
 	
 }
+function ToneSound(context,time,pitch)
+{
+	var time = 100000;
+	var soundbuffer = context.createBuffer(1,time,context.sampleRate);
+	var data = soundbuffer.getChannelData(0);
+	var frequency = pitch/context.sampleRate
+	for(var i = 0;i < time;i++)
+	{
+		data[i] = Math.sin(frequency * i)
+	}
+	return soundbuffer;
+	
+}
+
+
+
+
+*/
