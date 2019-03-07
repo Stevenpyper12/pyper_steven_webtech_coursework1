a603913f2903a492e6f76596a6ded8ec08e4571c
steven <stevenpyper12@gmail.com>
2019-03-07 17:48:35 +0000

    draft report and finalising layout changes

--- changecipher.js
+++ changecipher.js
@@ -21,12 +21,14 @@ function changecipher(){
 }
 
 function Caesarselected(){
-	document.getElementById("MethodSelector").innerHTML = "<select id = 'selectmethodbox' onchange='changemethod();'><option value='0' selected disabled>Please Chose Your Cipher Method</option><option value='1'>Encrypt</option><option value='2'>Decrypt</option><option value='3'>Crack</option><option value='4'>ROT13 Encrypt</option><option value='5'>ROT13 Decrypt</option></select>"
-
+	document.getElementById("MethodSelector").innerHTML = "Here you can choose how you would like the Cipher to work, would you like it to encrypt or decrypt? Click the dropdown box to see options!"
+	document.getElementById("MethodSelector").innerHTML += "<select id = 'selectmethodbox' onchange='changemethod();'><option value='0' selected disabled>Please Chose Your Cipher Method</option><option value='1'>Encrypt</option><option value='2'>Decrypt</option><option value='3'>Crack</option><option value='4'>ROT13 Encrypt</option><option value='5'>ROT13 Decrypt</option></select>"
+	
 }
 
 function Subsititionselected(){
-	document.getElementById("MethodSelector").innerHTML = "<select id = 'selectmethodbox' onchange='changemethod();'><option value='0' selected disabled>Please Chose Your Cipher Method</option><option value='1'>Encrypt</option><option value='2'>Decrypt</option></select>"
+	document.getElementById("MethodSelector").innerHTML = "Here you can choose how you would like the Cipher to work, would you like it to encrypt or decrypt? Click the dropdown box to see options!"
+	document.getElementById("MethodSelector").innerHTML += "<select id = 'selectmethodbox' onchange='changemethod();'><option value='0' selected disabled>Please Chose Your Cipher Method</option><option value='1'>Encrypt</option><option value='2'>Decrypt</option></select>"
 /*
 	document.getElementById("output").innerHTML += "<p><input type='text' id='inputtext' value=''> enter the text you want converted </p>"
 	document.getElementById("output").innerHTML += "<button onclick='Subsitition(\"0\")'>Setup New Alphabet and encrypt</button>"	
@@ -35,7 +37,8 @@ function Subsititionselected(){
 }
 
 function Morseselected(){
-	document.getElementById("MethodSelector").innerHTML = "<select id = 'selectmethodbox' onchange='changemethod();'><option value='0' selected disabled>Please Chose Your Cipher Method</option><option value='1'>Convert To Morse</option><option value='2'>Convert To Text</option></select>"
+	document.getElementById("MethodSelector").innerHTML = "Here you can choose if you want to go from text to morse, or morse to text. Click the dropdown box to see options!"
+	document.getElementById("MethodSelector").innerHTML += "<select id = 'selectmethodbox' onchange='changemethod();'><option value='0' selected disabled>Please Chose Your Cipher Method</option><option value='1'>Convert To Morse</option><option value='2'>Convert To Text</option></select>"
 /*
 	document.getElementById("output").innerHTML = "<p><input type='text' id='inputtext' value=''> enter the text you want converted into morse</p>"
 	document.getElementById("output").innerHTML += "<p><input type='text' id='inputmorse' value=''> enter the morse you want converted into text</p>"
